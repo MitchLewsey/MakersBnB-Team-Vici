1106951db4082b89637bc65f3ff84fc854c0bbc9
paulflieshman <paulfury@gmail.com>
2026-01-20 11:30:40 +0000

    updated users_seeds file as per PR comments

--- seeds/users_seeds.sql
+++ seeds/users_seeds.sql
@@ -9,9 +9,9 @@ DROP TABLE IF EXISTS users;
 -- Then, we recreate them
 CREATE TABLE users (
     id SERIAL PRIMARY KEY, 
-    name VARCHAR(255),
-    email VARCHAR(255),
-    password_hash VARCHAR(255)
+    name VARCHAR(255) UNIQUE NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL
     );
 
 -- Finally, we add any records that are needed for the tests to run
@@ -19,3 +19,4 @@ INSERT INTO users (name, email, password_hash) VALUES
 ('Fred Smith', 'fred@smith.com', 'fred123');
 ('Sam Jones', 'sam@jones.com', 'sam123');
 ('Matthew Wiggans', 'matthew@wiggans.com', 'matthew123');
+('Dani Rojas', 'dani@rojas.com', 'rojas123');
