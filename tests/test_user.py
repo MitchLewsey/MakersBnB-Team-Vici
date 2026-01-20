31e3491b779f56cdaf5df7019ea2a85a6ebbd66a
Jonathan-Smith9410 <jsmith300599@googlemail.com>
2026-01-20 17:40:39 +0000

    Tests successful for class User

--- tests/test_user.py
+++ tests/test_user.py
@@ -6,9 +6,10 @@ Constructs with name and parameters
 
 def test_constructs():
     user = User(1, "Test Name", "Test Email", "Test Password_Hash")
-    assert User.id == 1
-    assert User.name == "Test Name"
-    assert User.genre == "Test Genre"
+    assert user.id == 1
+    assert user.name == "Test Name"
+    assert user.email == "Test Email"
+    assert user.password_hash == "Test Password_Hash"
 
 def test_user_format():
     user = User(1, "Test Name", "Test Email", "Test Password_Hash")
