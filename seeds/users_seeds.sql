18e7deebae3e995f0d7241106b138be72cc2df61
Mitchell <mitch.lewsey@gmail.com>
2026-01-20 12:21:30 +0000

    setup seed_dev_database.py, created listings_seed.sql and edited users_seeds.sql

--- seeds/users_seeds.sql
+++ seeds/users_seeds.sql
@@ -4,19 +4,19 @@
 -- database state, and that tests don't interfere with each other.
 
 -- First, we must delete (drop) all our tables
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users CASCADE;
 
 -- Then, we recreate them
 CREATE TABLE users (
     id SERIAL PRIMARY KEY, 
-    name VARCHAR(255) UNIQUE NOT NULL,
-    email VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
     password_hash VARCHAR(255) NOT NULL
     );
 
 -- Finally, we add any records that are needed for the tests to run
 INSERT INTO users (name, email, password_hash) VALUES 
-('Fred Smith', 'fred@smith.com', 'fred123');
-('Sam Jones', 'sam@jones.com', 'sam123');
-('Matthew Wiggans', 'matthew@wiggans.com', 'matthew123');
+('Fred Smith', 'fred@smith.com', 'fred123'),
+('Sam Jones', 'sam@jones.com', 'sam123'),
+('Matthew Wiggans', 'matthew@wiggans.com', 'matthew123'),
 ('Dani Rojas', 'dani@rojas.com', 'rojas123');
