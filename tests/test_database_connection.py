9c002c10f864a62d70b3d17df42ecdc23f3f7fff
paulflieshman <paulfury@gmail.com>
2026-01-20 12:40:35 +0000

    fixed the users_seeds.sql and added tests in tests_database_connection.py

--- tests/test_database_connection.py
+++ tests/test_database_connection.py
@@ -6,16 +6,20 @@ I get some records back
 """
 def test_database_connection(db_connection):
     # Seed the database with some test data
-    db_connection.seed("seeds/database_connection.sql")
+    db_connection.seed("seeds/users_seeds.sql")
 
-    # Insert a new record
-    db_connection.execute("INSERT INTO test_table (name) VALUES (%s)", ["second_record"])
+    # # Insert a new record
+    # db_connection.execute("INSERT INTO users (name, email, password_hash) VALUES (%s)", ["Allen Iverson", "allen@iverson.com", "allen123"])
 
     # Retrieve all records
-    result = db_connection.execute("SELECT * FROM test_table")
+    result = db_connection.execute("SELECT * FROM users")
 
     # Assert that the results are what we expect
     assert result == [
-        {"id": 1, "name": "first_record"},
-        {"id": 2, "name": "second_record"}
+        {"id": 1, "name": "Fred Smith", "email": "fred@smith.com", "password_hash": "fred123" },
+        {"id": 2, "name": "Sam Jones", "email": "sam@jones.com", "password_hash": "sam123" },
+        {"id": 3, "name": "Matthew Wiggans", "email": "matthew@wiggans.com", "password_hash": "matthew123" },
+        {"id": 4, "name": "Dani Rojas", "email": "dani@rojas.com", "password_hash": "dani123" },
+        # {"id": 5, "name": "Allen Iverson", "email": "allen@iverson.com", "password_hash": "allen123" }
+
     ]
