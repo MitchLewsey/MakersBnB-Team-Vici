4d31bf1af180a112b091731feabae4a042e1ea42
Rhia <rhiafhunte@gmail.com>
2026-01-21 11:31:18 +0000

    Added routes for Request a booking page

--- app.py
+++ app.py
@@ -33,7 +33,40 @@ def login():
         return render_template("test_listings.html"), 200
     else:
         return "Error: Invalid username or password", 401
+    
 
+@app.route('/book', methods=['POST'])
+def request_booking():
+    listing_id = request.form.get('listing_id')
+    start_date = request.form.get('start_date')
+    end_date = request.form.get('end_date')
+
+    if not listing_id or not start_date or not end_date:
+        return "Error: missing or invalid date. Please select a valid date.", 405
+    
+    connection = get_flask_database_connection(app)
+
+    connection.execute(
+        "INSERT INTO bookings (listing_id, start_date, end_date) VALUES (%s, %s, %s)",
+        [listing_id, start_date, end_date]
+    )
+
+    return "Your booking request has been submitted successfully.", 200
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# FYI - KEEP THIS AT THE BOTTOM OF THE CODEBASE
 # These lines start the server if you run this file directly
 # They also start the server configured to use the test database
 # if started in test mode.
